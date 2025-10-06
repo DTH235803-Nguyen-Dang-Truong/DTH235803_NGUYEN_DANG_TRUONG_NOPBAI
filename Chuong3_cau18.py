65037344cd130b7f03361c937b9f0e13f66ee363
DTH235803_Nguyễn_Đăng_Trường <truong_dth235803@student.agu.edu.vn>
2025-10-06 15:28:24 +0700

    Nop bai tap Python

---
+++ Chuong3_cau18.py
@@ -0,0 +1,19 @@
+
+n = int(input("Nhập chiều cao n: "))
+
+print("Hình 1:")
+for i in range(n, 0, -1):
+    print("* " * i)
+
+print("\nHình 2:")
+for i in range(1, n+1):
+    print("* " * i)
+
+print("\nHình 3:")
+for i in range(n, 0, -1):
+    print("  " * (n-i) + "* " * i)
+
+print("\nHình 4:")
+for i in range(1, n+1):
+    print("  " * (n-i) + "* " * i)
+
